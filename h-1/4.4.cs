f7ee3f6fd16cb4c94ca150cc12bdbc599b5f34fa
cesyamagdalena <cesyamagdalena@gmail.com>
2024-11-22 08:08:18 +0700

    Create 4.4.cs

---
+++ h-1/4.4.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+  static void Main(String[] args)
+  {
+    int i = 1;
+    do
+    {
+      Console.WriteLine("\n Nilai i= " + i);
+      i++;
+    }
+      while (i <= 5);
+      Console.Read();
+  }
+}
