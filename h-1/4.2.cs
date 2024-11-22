98a4f6c35e86316825f3d740401e92b4cbd3f053
cesyamagdalena <cesyamagdalena@gmail.com>
2024-11-22 08:01:49 +0700

    Create 4.2.cs

---
+++ h-1/4.2.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+  public static void Main(String[] args)
+  {
+    for (int angka = 10; angka >= 1; angka--)
+    {
+      Console.WriteLine("\n Angka Ke -" + angka.ToString());
+    }
+      Console.Read();
+  }
+}
