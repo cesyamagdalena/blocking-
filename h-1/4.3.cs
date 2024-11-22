28cc05cee7b5303514d259fcaaeaa0a300e3ab36
cesyamagdalena <cesyamagdalena@gmail.com>
2024-11-22 08:05:48 +0700

    Create 4.3.cs

---
+++ h-1/4.3.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+  public static void Main(String[] args)
+    {
+      int i = 1;
+      while (i <= 12)
+      {
+        Console.WriteLine("\n Belajar C# "+i);
+        i++;
+      } 
+        Console.Read();
+    }
+}
